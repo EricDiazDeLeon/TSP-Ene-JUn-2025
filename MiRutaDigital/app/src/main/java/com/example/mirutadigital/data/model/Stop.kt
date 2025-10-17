8ebf16be6529787c63f790041fb2651153c9be2d
artacas <167118692+artacas@users.noreply.github.com>
2025-10-16 22:19:28 -0600

    Delete MiRutaDigital directory
    ahorita lo vuelvo a subir

--- MiRutaDigital/app/src/main/java/com/example/mirutadigital/data/model/Stop.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mirutadigital.data.model
-
-import com.google.android.gms.maps.model.LatLng
-
-data class Stop(
-    val id: String,
-    val name: String,
-    val location: LatLng
-)
--- MiRutaDigital/app/src/main/java/com/example/mirutadigital/data/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mirutadigital.data.model
-
-data class User(
-    val id: Int,
-    val name: String,
-    val email: String,
-    val phone: String = ""
-)
